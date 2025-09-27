fb12e0bfc3dfa6d291ae4344af7fc37ff4e9ff1f
danhoernchen <191582347+danhoernchen@users.noreply.github.com>
2025-09-27 10:49:28 +0200

    updated for deployment

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
+const path = require("path");
 // enable CORS
 app.use(cors());
 // set the port on which our app wil run
@@ -14,6 +15,21 @@ const port = process.env.PORT || 8088;
 //   res.send("Hello World!");
 // });
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+app.get("/style.css", (req, res) => {
+  res.setHeader("Content-Type", "text/css");
+  res.sendFile(path.join(__dirname, "style.css"));
+});
+app.get("/script.js", (req, res) => {
+  res.setHeader("Content-Type", "text/javascript");
+  res.sendFile(path.join(__dirname, "script.js"));
+});
+app.get("/loading.svg", (req, res) => {
+  res.setHeader("Content-Type", "image/svg+xml");
+  res.sendFile(path.join(__dirname, "loading.svg"));
+});
 app.get("/search/:id", (req, res) => {
   // replace with a custom URL as required
   const backendUrl = `https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=${req.params.id}`;
